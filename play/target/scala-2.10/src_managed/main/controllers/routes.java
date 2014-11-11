99bc0a418f3c785cfabbd2d7d11a00e6b4f0cf18
Tobias Schiller <toeber@gmail.com>
2014-11-11 16:25:29 +0100

    changed play template

---
+++ play/target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:/Users/tobias/Documents/Studium/Vorlesungen/WebTech/buechern/play3/conf/routes
+// @HASH:a19d08a3f65545aa72fc06c7532681495f05e295
+// @DATE:Tue Nov 11 16:02:48 CET 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
