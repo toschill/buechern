5fef4bb65aadb6e8fcaa4b77995cf4095e444c9c
unknown <f.ambrosi@web.de>
2014-11-12 13:06:15 +0100

    change profile for play

--- buechern/app/controllers/Application.java
+++ buechern/app/controllers/Application.java
@@ -10,5 +10,9 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render());
     }
+    
+    public static Result profile() {
+		return ok(profile.render());
+	}
 
 }
