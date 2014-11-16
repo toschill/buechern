1447401cddbebfe162ba92f1f08d3455e9dccb5d
unknown <f.ambrosi@web.de>
2014-11-16 13:25:59 +0100

    add routes

--- buechern/app/controllers/Application.java
+++ buechern/app/controllers/Application.java
@@ -10,8 +10,15 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render());
     }
-//    public static Result  profile() {
-//		return ok(profile.render());
-//	}
-
+    public static Result  profile() {
+		return ok(profile.render());
+	}
+    
+    public static Result changeUserData(){
+    	return ok(userDatenAendern.render());
+    }
+    
+	public static Result verkaufen(){
+		return ok(verkaufen.render());
+   }
 }
